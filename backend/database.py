305621111528a40af40d7de352a28cb536fa72b6
cdcg419 <82683116+cdcg419@users.noreply.github.com>
2025-09-17 02:05:06 -0500

    Update database.py

--- backend/database.py
+++ backend/database.py
@@ -1,12 +1,18 @@
-import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-URL_DATABASE = os.getenv("AZURE_MYSQL_URL")
+URL_DATABASE = (
+    "mysql+pymysql://Api_DB:Database_tesis123"
+    "@database-tesis.mysql.database.azure.com:3306/api_db?charset=utf8mb4"
+)
 
-SSL_CERT_PATH = os.getenv("SSL_CERT_PATH")
+SSL_CERT_PATH = "/home/site/wwwroot/certs/DigiCertGlobalRootG2.crt.pem"  # Ruta en Azure
 
-ssl_args = {"ssl": {"ca": SSL_CERT_PATH}} if SSL_CERT_PATH else {}
+ssl_args = {
+    "ssl": {
+        "ca": SSL_CERT_PATH
+    }
+}
 
 engine = create_engine(URL_DATABASE, connect_args=ssl_args)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
